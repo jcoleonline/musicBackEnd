d2ee90569394b6efef68033c88262a528500e6da
jcoleonline <jcoleonlinellc@gmail.com>
2023-04-01 11:54:06 -0500

    2nd commit

--- seeders/20230401145430-user.js
+++ seeders/20230401145430-user.js
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const http = require ('http')
 const hostname = '127.0.0.1';
 const port = 3000;
+const Sequelize = require('sequelize');
+const { user } = require('./models');
+const { artist } = require('./models');
+const { album } = require('./models');
 
 const express = require('express');
 const app = express();
@@ -12,6 +16,25 @@ app.set('view engine', 'html');
 
 const server = http.createServer(app);
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.get('/users', async (req, res) => {
+  const users = await user.findAll();
+  res.json(users);
+});
+
+app.get('/artists', async (req, res) => {
+  const artists = await artist.findAll();
+  res.json(artists);
+});
+
+app.get('/albums', async (req, res) => {
+  const albums = await album.findAll();
+  res.json(albums);
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
