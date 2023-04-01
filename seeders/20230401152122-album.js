45d5fcdf65863a8e7a27ed30116d282e535e572e
jcoleonline <jcoleonlinellc@gmail.com>
2023-04-01 10:43:30 -0500

    ramons edit

---
+++ seeders/20230401152122-album.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('albums', [{
+      name: 'HOPE',
+      year: '2023',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
