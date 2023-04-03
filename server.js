c8a357ece85a64be559d3af2d41e62840e9c3700
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-04-03 12:25:30 -0400

    finished crud functionality

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('home');
 });
 
-app.post('/users', async (req, res) => {
+app.post('/users', async (req, res) => {   //<-- creates new user
     const { username, email, password } = req.body;
     const newUser = await user.create({
         username,
@@ -40,17 +40,85 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-app.get('/users/by-username', async (req, res) => {
-    const username = await user.findAll({
-        attributes: ['username']
+app.get('/users/:id', async (req, res) => {  //<--- gets user by id
+  try{
+    const oneUser = await user.findByPk(req.params.id);
+    res.json(oneUser);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({
+      message: 'User not found'
     });
-    res.json(username)
+  }
 });
 
+app.post('/users/:id', async (req, res) => { //<-- updates user by id
+  const { id } = req.params;
+  const updatedUser = await user.update(req.body, {
+    where: {
+      id
+    }
+  });
+  res.json(updatedUser);
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedUser = await user.destroy({
+    where: {
+      id
+    }
+  });
+  res.json(deletedUser);
+})
 
 app.get('/artists', async (req, res) => {
   const artists = await artist.findAll();
   res.json(artists);
+});
+
+app.post('/artists', async (req, res) => {
+  const { name, genre } = req.body;
+  const newArtist = await artist.create({
+    name,
+    genre
+  });
+  res.json({
+    id: newArtist.id,
+    name: newArtist.name
+  });
+});
+
+app.get('/artists/:id', async (req, res) => {
+  try{
+    const oneArtist = await artist.findByPk(req.params.id);
+    res.json(oneArtist);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({
+      message: 'Artist not found.'
+    });
+  }
+});
+
+app.post('/artists/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedArtist = await artist.update(req.body, {
+    where: {
+      id
+    }
+  });
+  res.json(updatedArtist);
+});
+
+app.delete('/artists/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedAritst = await artist.destroy({
+    where: {
+      id
+    }
+  });
+  res.json(deletedAritst);
 });
 
 app.get('/albums', async (req, res) => {
@@ -58,7 +126,53 @@ app.get('/albums', async (req, res) => {
   res.json(albums);
 });
 
-app.get('/artists/albums', async (req, res) =>{
+app.post('/albums', async (req, res) => {
+  const { name, year, artistid } = req.body;
+  const newAlbum = await album.create({
+    name,
+    year,
+    artistid
+  });
+  res.json({
+    id: newAlbum.id,
+    name: newAlbum.name
+  });
+});
+
+app.get('/albums/:id', async (req, res) => {
+  try{
+    const oneAlbum = await album.findByPk(req.params.id);
+    res.json(oneAlbum);
+  } catch (e) {
+    console.log(e);
+    res.status(404).json({
+      message: 'Album not found'
+    });
+  }
+});
+
+app.post('/albums/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedAlbum = await album.update(req.body, {
+    where: {
+      id
+    }
+  });
+  res.json(updatedAlbum);
+});
+
+app.delete('/albums/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedAlbum = await album.destroy({
+    where: {
+      id
+    }
+  });
+  res.json(deletedAlbum);
+});
+
+
+app.get('/artists/albums', async (req, res) =>{  //<-- foreign key handler
     const artistAlbum = await artist.findAll({
         include: [{
             model: album
